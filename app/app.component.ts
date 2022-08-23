db6e4cd5116a4b9eba311ab9aaaf280067795728
lcvalve <32527660+lcvalve@users.noreply.github.com>
2022-08-22 21:29:12 -0700

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+// handleRating(rate: number){
+//   alert('The user selected a rating of : ' + rate + ' stars');
+// }
+
+}
